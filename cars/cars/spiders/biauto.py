33f6930e5ba002c3a05e5e10bdc1b9bae216a007
Xinfan Meng <mxf3306@gmail.com>
2012-02-26 18:53:29 +0800

    minor fixes

--- cars/cars/spiders/biauto.py
+++ cars/cars/spiders/biauto.py
@@ -1,21 +1,11 @@
+#encoding:utf-8
 """
 Scrape biatuo car recall news
 """
-from scrapy.item import Item, Field
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.selector import HtmlXPathSelector
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
-
-class CarNews(Item):
-    """
-    Data structure for car news
-    """
-    url = Field()
-    title = Field()
-    topic = Field()
-    time = Field()
-    source = Field()
-    content = Field()
+from carnews import CarNews
 
 class CarNewsSpider(CrawlSpider):
 
@@ -26,30 +16,31 @@ class CarNewsSpider(CrawlSpider):
             Rule(
                 SgmlLinkExtractor(
                     #allow=r".*",
-                    restrict_xpaths='//div[@id="newslist"]'),
-                    #restrict_xpaths='//div[@class="line_box all_newslist mainlist_box"]'),
+                    #use the following xpath to crawl the start page
+                    #restrict_xpaths='//div[@id="newslist"]'),
+                    #use the following xpath to crawl multiple pages
+                    restrict_xpaths='//div[@class="line_box all_newslist mainlist_box"]'),
                 callback="parse_news",
                 follow=True),
             )
 
     def parse_news(self, response):
+        """
+        Extract one item from one page
+        """
         x = HtmlXPathSelector(response)
-
-        #print "----", self.allowed_domains
-
         if "recall_" not in response.url:
             return None
 
         news = CarNews()
         news['url'] = response.url
         news['title'] = x.select('//h1[@class="con"]//text()').extract()
-        news['topic'] = ""
+        news['topic'] = u"汽车"
         news['time'] = x.select('//li[@id="time"]/text()').extract()
-        news['source'] = ""
+        news['source'] = u"易车网"
         # Use string() or //text() to select nested text
-        news['content'] = \
-                [e.strip() for e in x.select('//div[@class="con_main"]//text()').extract()]
-        print news['title']
+        # Temporarily disable the content field to simplify output
+        #news['content'] = [e.strip() for e in x.select('//div[@class="con_main"]//text()').extract()]
 
         return news
 
--- cars/cars/spiders/qiche365.py
+++ cars/cars/spiders/qiche365.py
@@ -1,3 +1,4 @@
+#encoding: utf-8
 """
 Scrape qiche365 car recall news
 """
@@ -5,6 +6,8 @@ from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.selector import HtmlXPathSelector
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from carnews import CarNews
+from scrapy.utils.url import urljoin_rfc
+from scrapy.utils.response import get_base_url
 
 
 class Qiche365Spider(CrawlSpider):
@@ -25,16 +28,25 @@ class Qiche365Spider(CrawlSpider):
         self.news_list = []
 
     def parse_news(self, response):
+        """
+        Extract multiple items from a list in each page. Note that
+        we don't follow the links to extract the news content.
+        """
         x = HtmlXPathSelector(response)
+        base_url = get_base_url(response)
 
         self.news_list = []
         #nested selectors
         for recall_line in x.select('//div[@class="recall_Line"]'):
             news = CarNews()
-            news['url'] = recall_line.select('div[@class="recall_title"]/a/@href').extract()
             news['title'] = recall_line.select('div[@class="recall_title"]/a/text()').extract()
+            if not news['title']:
+                continue
+            relative_url = recall_line.select('div[@class="recall_title"]/a/@href').extract()[0]
+            news['url'] = urljoin_rfc(base_url, relative_url)
             news['time'] = recall_line.select('div[@class="recall_time"]/text()').extract()
-            news['source'] = ""
+            news['source'] = u"中国汽车召回网"
+            news['topic'] = u"汽车"
             self.news_list.append(news)
 
         return self.news_list
